9dc0bd1104fb82144fee23589088388fd9944f3b
minfantemonterrey-collab <minfantemonterrey@gmail.com>
2026-09-19 16:09:42 -0500

    se le agrega una melodía original al proyecto para que sea mas efectiva

--- main.ts
+++ main.ts
@@ -3,4 +3,5 @@ input.onGesture(Gesture.Shake, function () {
     aleatorio = 0
     aleatorio += randint(1, 1000)
     basic.showNumber(aleatorio)
+    music.play(music.stringPlayable("C C E E D B G E ", 120), music.PlaybackMode.UntilDone)
 })
