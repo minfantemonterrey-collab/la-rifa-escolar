a5c731f751f2c1c31c99f0ceb86d5b33fbf5278c
minfantemonterrey-collab <minfantemonterrey@gmail.com>
2026-09-19 15:51:14 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+let aleatorio = 0
+input.onGesture(Gesture.Shake, function () {
+    aleatorio = 0
+    aleatorio += randint(1, 100)
+    basic.showNumber(aleatorio)
 })
