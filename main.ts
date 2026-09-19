92c45dc7806cb3835139f5d92a4d070eb85002cb
minfantemonterrey-collab <minfantemonterrey@gmail.com>
2026-09-19 16:05:46 -0500

    se le aumento el valor de escogimiento para que las probabilidades de que un numero salga varias veces sea de un 10%

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let aleatorio = 0
 input.onGesture(Gesture.Shake, function () {
     aleatorio = 0
-    aleatorio += randint(1, 100)
+    aleatorio += randint(1, 1000)
     basic.showNumber(aleatorio)
 })
